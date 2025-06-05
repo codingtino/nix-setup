9d028570a51867b449f10212f226ac9971f600ae
Tino Naumann <172442418+codingtino@users.noreply.github.com>
2025-06-05 23:18:07 +0200

    working with prettier and openai plugins vor vscode

--- home/t.naumann.nix
+++ home/t.naumann.nix
@@ -12,13 +12,26 @@
   programs.vscode = {
     enable = true;
     profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        esbenp.prettier-vscode
+        ms-azuretools.vscode-docker
+        mechatroner.rainbow-csv
+        ms-vscode-remote.remote-ssh
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "chatgpt";
+          publisher = "openai";
+          version = "0.1.1741291060"; # Use the latest version you want
+          sha256 = "N5MJKY0DTLCLHPaVB/k6o1j8ev7Z4VNOfYC6NU9g9RE="; # Replace if version changes
+        }
+      ];
+
       userSettings = {
         "prettier.prettierPath" =
           "${pkgs.vscode-extensions.esbenp.prettier-vscode}/share/vscode/extensions/esbenp.prettier-vscode/node_modules/prettier";
       };
     };
   };
-
 
   programs.eza = {
     enable = true;
--- hosts/common/common-packages.nix
+++ hosts/common/common-packages.nix
@@ -24,30 +24,6 @@ in
     unzip
     watch
     wget
-          (vscode-with-extensions.override {
-            vscodeExtensions = builtins.concatLists [
-              (with pkgs.vscode-extensions; [
-                esbenp.prettier-vscode
-                ms-azuretools.vscode-docker
-                mechatroner.rainbow-csv
-                ms-vscode-remote.remote-ssh
-              ])
-              (pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-                {
-                  name = "remote-ssh-edit";
-                  publisher = "ms-vscode-remote";
-                  version = "0.47.2";
-                  sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-                }
-                {
-                  name = "chatgpt";
-                  publisher = "openai";
-                  version = "0.1.1741291060";
-                  sha256 = "N5MJKY0DTLCLHPaVB/k6o1j8ev7Z4VNOfYC6NU9g9RE=";
-                }
-              ])
-            ];
-          })
 
     # requires nixpkgs.config.allowUnfree = true;
 #    vscode-extensions.ms-vscode-remote.remote-ssh
